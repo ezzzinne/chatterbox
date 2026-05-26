bb262ec15c7d17d3af1def18776c1f43d46208d1
ezzzinne <cynthia.nwani017@gmail.com>
2026-05-26 10:26:47 +0100

    chore: integrate supabase and env variables into project

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  devIndicators: false,
+  typedRoutes: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  reactCompiler: true,
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
 };
 
 export default nextConfig;
